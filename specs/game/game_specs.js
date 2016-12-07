285a685fe840599f8c672a1f9a2de6a4669d8024
Dan Reich <dancreich@gmail.com>
2016-12-07 12:05:32 -0500

    Refactor Pawn class to use proper method defs.

---
+++ specs/game/game_specs.js
@@ -0,0 +1,6 @@
+var test = require('tape');
+// var Game = require('../../src/Game.js');
+
+// test('Game.newGame initializes new board and two players.', (t) => {
+//
+// });
--- src/js/pieces/Pawn.js
+++ src/js/pieces/Pawn.js
@@ -6,68 +6,66 @@ var Utils = require('../Utils');
 module.exports = class Pawn extends Piece {
   constructor (pos, color, board) {
     super(pos, color, board);
+
     this.hasMoved = false;
-
     this.directionMod = (this.color === 'white') ? 1 : -1;
-    this.moveDirections = []
-
-
-    /**
-     * Find diagonal capture moves.
-     * @return {Array} Array of move algs.
-     */
-    this._captureMoves = () => {
-
-      var moveList = [];
-      var rank = this.pos.rank + (1 * this.directionMod);
-      var leftCapture = Utils.toAlg({rank, file: this.pos.file - 1});
-      var rightCapture = Utils.toAlg({rank, file: this.pos.file + 1});
-
-      if (this.board.getPiece(leftCapture)) {
-        moveList.push(leftCapture);
-      }
-
-      if (this.board.getPiece(rightCapture)) {
-        moveList.push(rightCapture);
-      }
-
-      return moveList;
-    };
-
-    /**
-     * Returns a list of possible legal moves for the piece. Calls captureMoves
-     * to check for special pawn diagonal captures.
-     * @returns {Array} Array of possible legal move algs.
-     */
-    this.moves = () => {
-
-      var dest, rank;
-      var boundaryRank = (this.color === 'white') ? 7 : 0;
-      var moveList = [];
-
-      if ((this.color === 'white' && this.pos.rank < boundaryRank) ||
-          (this.color === 'black' && this.pos.rank > boundaryRank)) {
-
-        rank = this.pos.rank + (1 * this.directionMod)
-        dest = Utils.toAlg({rank, file: this.pos.file});
-
-        if (board.isEmpty(dest)) {
-          moveList.push(dest);
-        }
-      }
-
-      if (!this.hasMoved) {
-        rank = this.pos.rank + (2 * this.directionMod)
-        dest = Utils.toAlg({rank, file: this.pos.file})
-
-        if (board.isEmpty(dest)) {
-          moveList.push(dest);
-        }
-      }
-
-      moveList = moveList.concat(this._captureMoves());
-
-      return moveList;
-    };
   }
+
+
+  /**
+  * Returns a list of possible legal moves for the piece. Calls captureMoves
+  * to check for special pawn diagonal captures.
+  * @returns {Array} Array of possible legal move algs.
+  */
+  moves() {
+    var dest, rank;
+    var boundaryRank = (this.color === 'white') ? 7 : 0;
+    var moveList = [];
+
+    if ((this.color === 'white' && this.pos.rank < boundaryRank) ||
+    (this.color === 'black' && this.pos.rank > boundaryRank)) {
+
+      rank = this.pos.rank + (1 * this.directionMod)
+      dest = Utils.toAlg({rank, file: this.pos.file});
+
+      if (this.board.isEmpty(dest)) {
+        moveList.push(dest);
+      }
+    }
+
+    if (!this.hasMoved) {
+      rank = this.pos.rank + (2 * this.directionMod)
+      dest = Utils.toAlg({rank, file: this.pos.file})
+
+      if (this.board.isEmpty(dest)) {
+        moveList.push(dest);
+      }
+    }
+
+    moveList = moveList.concat(this._captureMoves());
+
+    return moveList;
+  };
+
+
+  /**
+   * Find diagonal capture moves.
+   * @return {Array} Array of move algs.
+   */
+  _captureMoves() {
+    var moveList = [];
+    var rank = this.pos.rank + (1 * this.directionMod);
+    var leftCapture = Utils.toAlg({rank, file: this.pos.file - 1});
+    var rightCapture = Utils.toAlg({rank, file: this.pos.file + 1});
+
+    if (this.board.getPiece(leftCapture)) {
+      moveList.push(leftCapture);
+    }
+
+    if (this.board.getPiece(rightCapture)) {
+      moveList.push(rightCapture);
+    }
+
+    return moveList;
+  };
 };
