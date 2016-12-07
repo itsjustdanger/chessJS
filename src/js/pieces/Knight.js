60ed40e7d37aa736f07181f3fc54ddd42749600e
Dan Reich <dancreich@gmail.com>
2016-12-07 10:43:20 -0500

    Refactor knight moves and implement SteppingPiece parent class.

--- src/js/pieces/Knight.js
+++ src/js/pieces/Knight.js
@@ -1,32 +1,15 @@
 "use strict";
 
-var Piece = require('./Piece');
+var SteppingPiece = require('./SteppingPiece');
 var Utils = require('../Utils');
 
 var KNIGHT_OFFSETS = [[-1, -2], [1, -2], [-1, 2], [1, 2],
                       [-2, -1], [2, -1], [-2, 1], [2, 1]];
 
-module.exports = class Knight extends Piece {
+module.exports = class Knight extends SteppingPiece {
   constructor (pos, color, board) {
     super(pos, color, board);
 
-    this.moves = () => {
-      var dest, offset, i;
-      var moveList = [];
-
-      for (i = 0; i < KNIGHT_OFFSETS.length; i++) {
-        offset = KNIGHT_OFFSETS[i];
-        dest = Utils.toAlg({
-          rank: (this.pos.rank + offset[0]),
-          file: (this.pos.file + offset[1]),
-        });
-
-        if (dest && !this.occupiedByFriendly(dest)) {
-          moveList.push(dest);
-        }
-      }
-
-      return moveList;
-    };
+    this.movementOffsets = KNIGHT_OFFSETS;
   }
 };
---
+++ src/js/pieces/SteppingPiece.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var Piece = require('./Piece');
+var Utils = require('../Utils');
+
+module.exports = class SteppingPiece extends Piece {
+  constructor (pos, color, board) {
+    super(pos, color, board);
+
+    /**
+     * Implemented by child class
+     */
+    this.movementOffsets = [];
+
+
+    /**
+     * Finds all stepping moves based on provided offsets. Only applies offset once
+     * and omits squares occupied by a friendly piece.
+     */
+    this.moves = () => {
+      var dest, offset, i;
+      var moveList = [];
+
+      for (i = 0; i < this.movementOffsets.length; i++) {
+        offset = this.movementOffsets[i];
+        dest = Utils.toAlg({
+          rank: (this.pos.rank + offset[0]),
+          file: (this.pos.file + offset[1]),
+        });
+
+        if (dest && !this.occupiedByFriendly(dest)) {
+          moveList.push(dest);
+        }
+      }
+
+      return moveList;
+    }
+  }
+}
